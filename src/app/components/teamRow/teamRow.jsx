bba3b666cda20a226e97b1641347be7f94e2e8e2
Chris Burke <burkcules@Chriss-iMac.local>
2018-07-30 12:36:40 -0500

    adds event for clicking on a TeamRow

--- src/app/components/teamRow/teamRow.jsx
+++ src/app/components/teamRow/teamRow.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import './teamRow.css';
+import ReactGA from 'react-ga';
 
 class TeamRow extends Component {
   constructor(props) {
     super(props);
 
     // Bind functions
-    
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    ReactGA.event({
+      category: 'Navigation',
+      action: 'Clicked Team Row'
+    });
   }
 
   render() { 
 
     return (
-      <tr className='d-flex tr-hover' key={this.props.id}>
+      <tr className='d-flex tr-hover' key={this.props.id} onClick={this.handleClick}>
         <td className='col col-md-6'>{this.props.name}</td>
         <td className='col col-md-2'>{this.props.price}</td>
         <td className='col col-md-2'>{this.props.payout}</td>
