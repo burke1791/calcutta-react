bb2f266b107346bbc1516d67dbf31bc27891db54
Chris Burke <burke1791@gmail.com>
2019-02-02 13:55:47 -0600

    adds regex validation for team properties

--- src/app/components/addSportForm/addSportForm.jsx
+++ src/app/components/addSportForm/addSportForm.jsx
@@ -7,8 +7,37 @@ class AddSportForm extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      nodeId: '',
+      properties: '',
+      propertiesFlag: false,
+      teams: []
+    }
+
     // bind functions
+    this.onNodeIdChange = this.onNodeIdChange.bind(this);
+    this.onPropertiesChange = this.onPropertiesChange.bind(this);
     this.getTeamObjs = this.getTeamObjs.bind(this);
+  
+  }
+
+  onNodeIdChange(event) {
+    this.setState({nodeId: event.target.value});
+  }
+
+  onPropertiesChange(event) {
+    var properties = event.target.value;
+    var regex = /[a-z_0-9]{3,}/ig;
+
+    // perform some validation to ensure use adheres to the desired style
+
+    let result = properties.match(regex);
+
+    this.setState({properties: result});
+  }
+
+  getTeamObjs = () => {
+    return null;
   }
 
   render() {
@@ -16,11 +45,22 @@ class AddSportForm extends Component {
       <div className='add-sport-form container'>
         <form>
           <div className='row'>
-            <label htmlFor='sportNodeId'>Node ID:</label>
             <div className='col-2'>
-              <input type='text' className='form-control' id='sportNodeId' placeholder='node_id' />
+              <label className='add-sport-nodeId' htmlFor='sportNodeId'>Node ID:</label>
+            </div>
+            <div className='col-2'>
+              <input type='text' className='form-control' id='sportNodeId' onChange={this.onNodeIdChange} placeholder='node_id' />
             </div>
           </div>
+          <div className='row'>
+            <div className='col-2'>
+              <label className='team-properties' htmlFor='teamProperties'>Team Properties:</label>
+            </div>
+            <div className='col'>
+              <input type='text' className='form-control' id='teamProperties' onChange={this.onPropertiesChange} placeholder='comma separated team properties' />
+            </div>
+          </div>
+          <hr />
           {this.getTeamObjs()}
         </form>
       </div>
--- src/app/components/addSportTeam/addSportTeam.jsx
+++ src/app/components/addSportTeam/addSportTeam.jsx
@@ -5,12 +5,13 @@ class AddSportTeam extends Component {
   constructor(props) {
     super(props);
 
+
   }
 
   render() {
     return (
       <div className='team-info' id={this.props.teamKey}>
-
+        
       </div>
     );
   }
